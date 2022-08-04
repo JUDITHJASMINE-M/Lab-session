9cbeb86f10bb08e6ff9bd474859758e47d497962
JUDITHJASMINE-M <110027273+JUDITHJASMINE-M@users.noreply.github.com>
2022-08-04 17:56:04 +0530

    Add files via upload

---
+++ Program/Test3.java
@@ -0,0 +1,26 @@
+class Test3
+{
+public static void main(String[]args)
+{
+    int value = 40;
+    int[]arrvalue={20,30,40,50,60,70,80,90};
+   
+    System.out.println(arrvalue[4]);   
+    System.out.println(arrvalue[2]);
+    	
+    int leng=arrvalue.length;
+	
+	System.out.println(leng);
+      
+	  
+	for(int i=0; i < leng; i++)
+    {
+    System.out.println(arrvalue[i]); 
+
+
+    }	
+	  
+
+    }
+
+}
